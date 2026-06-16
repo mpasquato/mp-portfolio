ad7aa6ddb580471ce5352e5c74dbbce0dce36202
mpasquato <63077221+mpasquato@users.noreply.github.com>
2026-06-16 18:54:21 +0300

    Completed mob nav animation

--- js/navbar.js
+++ js/navbar.js
@@ -50,19 +50,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /* Mobile animation */
 
-// window.addEventListener("scroll", () => {
-//     const navbar = document.querySelector(".nav-mob");
-//     const trigger = document.getElementById("nav-appear");
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".nav-mob");
+    const trigger = document.getElementById("nav-appear");
 
-//     const triggerTop = trigger.getBoundingClientRect().top;
-//     console.log("triggerTop:", triggerTop);
+    const triggerTop = trigger.getBoundingClientRect().top;
+    console.log("triggerTop:", triggerTop);
 
-//     if (triggerTop <= 0) {
-//         console.log("ADD");
-//         navbar.classList.add("init-animation");
-//         navbar.classList.add("is-sticky");
-//     } else {
-//         console.log("REMOVE");
-//         navbar.classList.remove("is-sticky");
-//     }
-// });
+    if (triggerTop <= 0) {
+        navbar.classList.add("init-animation");
+        navbar.classList.add("is-sticky");
+    } else {
+        navbar.classList.remove("is-sticky");
+    }
+});
